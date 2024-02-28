57d8a2b825132f73d7062fb98808f3aad21dd3af
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-02-28 21:39:00 +0530

    Add files via upload

---
+++ src/Components/CartElement/CartElement.js
@@ -0,0 +1,47 @@
+import React , {useState} from 'recat'
+
+const CartElement = () => {
+    const [cartElements] = useState([
+        {
+
+            title: 'Colors',
+            
+            price: 100,
+            
+            imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+            
+            quantity: 2,
+            
+            },
+            
+            {
+            
+            title: 'Black and white Colors',
+            
+            price: 50,
+            
+            imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+            
+            quantity: 3,
+            
+            },
+            
+            {
+            
+            title: 'Yellow and Black Colors',
+            
+            price: 70,
+            
+            imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+            
+            quantity: 1,
+            
+            }
+    ])
+return <div>
+
+   
+    
+
+</div>
+}
