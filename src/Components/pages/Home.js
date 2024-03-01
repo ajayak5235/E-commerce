0c7c23f24831923f165885b96ee93192e20d3cb2
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-01 21:05:01 +0530

    Add files via upload

---
+++ src/Components/pages/Home.js
@@ -0,0 +1,18 @@
+const Home =  () =>{
+    return <>
+    <div>
+        <div> The Generics </div>
+            <h3>Get our Latest Album</h3>
+       
+    </div>
+     
+    <div>
+        <h3>Toure
+
+
+
+
+        </h3>
+    </div>
+    </>
+}
