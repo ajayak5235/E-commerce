6490b08c56d7c3f6f5ec313134760e611a4da96b
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-04 11:16:25 +0530

    Add files via upload

---
+++ src/Profile/ProfileForm.js
@@ -0,0 +1,55 @@
+import { useContext, useRef } from "react"
+import cartContext from "../store/Cart-Context";
+
+import { useNavigate } from "react-router-dom";
+import Login from "../pages/Login";
+
+const ProfileForm=()=>{
+
+    const newPasswordInputRef =useRef();
+
+    const authContext=useContext(cartContext)
+
+    const history=useNavigate()
+    const submitHandler=event=>{
+        event.preventDefault();
+
+        const enteredNewPassword=newPasswordInputRef.current.value;
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCLPdt6hd56ixmlKEzUMU06fngO3dzpmXc',{
+            method:'POST',
+            body: JSON.stringify({
+                idToken:authContext.token,
+                password:enteredNewPassword,
+                returnSecureToken:false
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res=>{
+            localStorage.removeItem('token')
+            authContext.logout()
+            history('/login')
+        })
+    }
+
+    return (
+        <>
+        { authContext.isLoggedIn && <form onSubmit={submitHandler} style={{margin:'auto',textAlign:"center"}}>
+            <div>
+                <label htmlFor="new-password">
+                    New Password
+                </label><br></br>
+                <input className="my-2" type="password" id="new-password" minLength={7} ref={newPasswordInputRef} />
+            </div>
+            <div>
+                <button>Change Password</button>
+            </div>
+        </form>}
+
+        {!authContext.isLoggedIn && <Login />}
+        </>
+    )
+}
+
+export default ProfileForm
